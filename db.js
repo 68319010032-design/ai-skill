6e8b42e419da6bdeda6f9f5a02e788a7b19d28a9
Aniwat <68319010032@htc.ac.th>
2026-01-12 11:38:40 +0700

    connect to railway mysql

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '1234',
-    database: process.env.MYSQLDATABASE || 'aidatabase',
-    port: process.env.MYSQLPORT || 3306
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  connectionLimit: 5
 });
 
 module.exports = pool;
